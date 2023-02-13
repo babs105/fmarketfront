8f38b8b9d57309e7d18eae465b6d879463a89602
babs105 <babacar.dieng4111@gmail.com>
2023-02-13 23:51:27 +0000

    first commit1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import React, { useState, useEffect } from "react";
-import {Routes, Route, NavLink, Outlet} from "react-router-dom";
+import { Routes, Route, NavLink, Outlet } from "react-router-dom";
 import bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
-import './App.css';
+import "./App.css";
 import Home from "./page/Home";
 import authService from "./services/authService";
-import Login from "./page/auth/Login";
-import Register from "./page/auth/Register";
-import PrivatedTraceRoute from "./routing/MainRoute";
-import PrivatedCarbuRoute from "./routing/PrivatedCarbuRoute";
-import EvenementList from "./components/trace/evenement/EvenementList";
-import AddEvenement from "./components/trace/evenement/AddEvenement";
-import Evenement from "./components/trace/evenement/Evenement";
-import Trace from "./components/trace/Trace";
 import MainRoute from "./routing/MainRoute";
 
 const App = () => {
   const [user, setUser] = useState(undefined);
 
   useEffect(() => {
-  const currentUser =  authService.getCurrentUser() ;
-    if(currentUser){
+    const currentUser = authService.getCurrentUser();
+    if (currentUser) {
       setUser(currentUser);
     }
-  },[]);
-  return (
-  <MainRoute user={user}/>
-  )
-}
+  }, []);
+  return <MainRoute user={user} />;
+};
 export default App;
